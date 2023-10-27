f565ac3fa5d83e1b55f46966ec57ac5bc80a5889
BorDevTech <bordevtech@icloud.com>
2023-10-27 07:56:09 -0400

    implementing base Window 11 design framework for desktop..continuing to icons and finishing Taskbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import "./App.css";
 import VirtualDisplay from "./Components/VirtualDisplay/VirtualDisplay";
 
 function App() {
   return (
     <>
       <VirtualDisplay />
-      <div style={{ border: "1px solid red" }}>
-        Display
-        <div style={{ border: "1px solid orange" }}>Screen</div>
-        <div style={{ border: "1px solid green" }}>
-          Taskbar
-          <div style={{ border: "1px solid blue" }}>Taskbar App</div>
-          <div style={{ border: "1px solid indigo" }}>Taskbar App</div>
-          <div style={{ border: "1px solid violet" }}>Taskbar App</div>
-          <div style={{ border: "1px solid pink" }}>Taskbar App</div>
-          <div style={{ border: "1px solid grey" }}>Taskbar App</div>
-        </div>
-      </div>
     </>
   );
 }
--- src/Components/VirtualDisplay/VirtualDisplay.tsx
+++ src/Components/VirtualDisplay/VirtualDisplay.tsx
@@ -1,11 +1,73 @@
 import * as CUR from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
 
 const VirtualDisplay = () => {
+  const randomAppAmount = { ideal: 20 * 8, max: 178 };
+
+  const randomExampleApps = [...Array(randomAppAmount.max)];
+
   return (
-    <>
-      <CUR.Button>Hello</CUR.Button>
-      <div>VirtualDisplay</div>
-    </>
+    <CUR.Grid
+      templateRows={`repeat(20,1fr)`}
+      as={CUR.Card}
+      h={innerHeight}
+      w={innerWidth}
+      style={{ border: "1px solid red" }}
+    >
+      <CUR.GridItem
+        rowSpan={19}
+        style={{ border: "1px solid orange" }}
+        as={CUR.Grid}
+        templateColumns={`repeat(20,1fr)`}
+        templateRows={`repeat(9,1fr)`}
+      >
+        {randomExampleApps.map((index, i) => (
+          <CUR.GridItem key={i} rowSpan={1} colSpan={1}>
+            <CUR.VStack gap={0}>
+              <CUR.Box
+                mt={2}
+                h={"60px"}
+                w={"60px"}
+                style={{ border: "1px solid blue" }}
+                as={CUR.Card}
+              >
+                <CUR.Icon justifyContent={"center"}>
+                  {/* Icon shall be either a community(backer), creator, generic design */}
+                </CUR.Icon>
+              </CUR.Box>
+              <CUR.Box>
+                {/* Should consist of App Name */}
+                {i}
+              </CUR.Box>
+            </CUR.VStack>
+          </CUR.GridItem>
+        ))}
+      </CUR.GridItem>
+      <CUR.GridItem
+        rowSpan={1}
+        as={CUR.Card}
+        borderTop={"2px"}
+        borderTopRadius={0}
+      >
+        <CUR.HStack justifyContent={"center"} mt={2} mb={2}>
+          <CUR.Box h={"40px"} w={"40px"} style={{ border: "1px solid blue" }}>
+            <CUR.Icon justifyContent={"center"}></CUR.Icon>
+          </CUR.Box>
+          <CUR.Box h={"40px"} w={"40px"} style={{ border: "1px solid indigo" }}>
+            <CUR.Icon justifyContent={"center"}></CUR.Icon>
+          </CUR.Box>
+          <CUR.Box h={"40px"} w={"40px"} style={{ border: "1px solid violet" }}>
+            <CUR.Icon justifyContent={"center"}></CUR.Icon>
+          </CUR.Box>
+          <CUR.Box h={"40px"} w={"40px"} style={{ border: "1px solid pink" }}>
+            <CUR.Icon justifyContent={"center"}></CUR.Icon>
+          </CUR.Box>
+          <CUR.Box h={"40px"} w={"40px"} style={{ border: "1px solid grey" }}>
+            <CUR.Icon justifyContent={"center"}></CUR.Icon>
+          </CUR.Box>
+        </CUR.HStack>
+      </CUR.GridItem>
+    </CUR.Grid>
   );
 };
 
