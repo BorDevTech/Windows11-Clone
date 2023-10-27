faa9dc042790a7e741a2b4d4254e0d36b4497d15
BorDevTech <bordevtech@icloud.com>
2023-10-27 05:17:49 -0400

    installed chakra.ui for rapid componentization

---
+++ src/Components/VirtualDisplay/VirtualDisplay.tsx
@@ -0,0 +1,12 @@
+import * as CUR from "@chakra-ui/react";
+
+const VirtualDisplay = () => {
+  return (
+    <>
+      <CUR.Button>Hello</CUR.Button>
+      <div>VirtualDisplay</div>
+    </>
+  );
+};
+
+export default VirtualDisplay;
