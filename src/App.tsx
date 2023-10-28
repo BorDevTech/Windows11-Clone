38b01f5d88a08bc9b5379ae7a6eaa69f879c378b
BorDevTech <bordevtech@icloud.com>
2023-10-28 04:50:46 -0400

    testbranch1

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div style={{ border: "1px solid red" }}>
-        Display
-        <div style={{ border: "1px solid orange" }}>Screen</div>
-        <div style={{ border: "1px solid green" }}>
-          Taskbar
-          <div style={{ border: "1px solid blue" }}>Taskbar App</div>
-          <div style={{ border: "1px solid indigo" }}>Taskbar App</div>
-          <div style={{ border: "1px solid violet" }}>Taskbar App</div>
-          <div style={{ border: "1px solid pink" }}>Taskbar App</div>
-          <div style={{ border: "1px solid grey" }}>Taskbar App</div>
-        </div>
-      </div>
-    </>
-  );
+  return <>testbranch1</>;
 }
 
 export default App;
